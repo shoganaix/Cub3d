7a910eaf227b5d26e43d1783ffe0a650a69510e1
Mayra Castrosqui Florian <macastro@c2r3s4.42madrid.com>
2024-12-17 16:53:38 +0100

    basics

--- cub3d.h
+++ cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macastro <macastro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:02:57 by msoriano          #+#    #+#             */
-/*   Updated: 2024/12/03 11:19:30 by msoriano         ###   ########.fr       */
+/*   Updated: 2024/12/17 16:44:50 by macastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,27 @@
 //# include "../libft/libft.h"
 
 
+# define EVENT_KEYPRESS		2
+# define EVENT_KEYRELEASE	3
+# define EVENT_MOUSEPRESS	4
+# define EVENT_MOUSERELEASE	5
+# define EVENT_MOUSEMOVE	6
+# define EVENT_CLOSEWINDOW	17
+# define MOUSE_LEFTCLICK	1
+# define MOUSE_RIGHTCLICK	2
+# define MOUSE_MIDCLICK		3
+# define MOUSE_SCROLLUP		4
+# define MOUSE_SCROLLDOWN	5
+
 /* KEYS DEfINED*/
 
-# define KEY_A			0
-# define KEY_S			1
-# define KEY_D			2
-# define KEY_W			13
+# define KEY_A			97
+# define KEY_S			KEY_A +  's' - 'a'
+# define KEY_D			KEY_A +  'd' - 'a'
+# define KEY_W			KEY_A +  'w' - 'a'
 
-# define KEY_ESC		53
+// # define KEY_ESC		53
+# define KEY_ESC		65307
 
 # define KEY_L			123
 # define KEY_R			124
--- src/playground.c
+++ src/playground.c
@@ -3,38 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   playground.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macastro <macastro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:10:39 by msoriano          #+#    #+#             */
-/*   Updated: 2024/12/03 13:34:21 by msoriano         ###   ########.fr       */
+/*   Updated: 2024/12/17 16:42:18 by macastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void	close_window(void *mlx)
+typedef struct	s_game {
+	void	*mlx;
+	void	*win;
+}				t_game;
+
+typedef struct	s_image
 {
-	(void)mlx;
-	exit(0);
+	void      *img;
+	char      *addr;
+	int       bits_per_pixel;
+	int       line_size;
+	int       endian;
+}   t_image;
+
+int	close_window(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->win);
+    // frees, destroys
+    exit(0);
 }
 
-int	key_press(int keycode, void *mlx)
+int	key_press_hndlr(int keycode, void *param)
 {
-    if (keycode == KEY_ESC) // Si la tecla es "Esc".
-        close_window(mlx); // Llamar a la función de cierre.
+    t_game *game;
+
+    game = (t_game *)param;
+    printf("key pressed %i\n", keycode);
+    if (keycode == KEY_ESC)
+        close_window(game);
     return (0);
 }
 
 int	main(void)
 {
-	void    *mlx;
-	void    *mlx_win;
+    t_game game;
+    // t_image img;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    // char	*relative_path = "./test.xpm";
+	// int		img_width;
+	// int		img_height;
+	
+    game.mlx = mlx_init();
+    game.win = mlx_new_window(game.mlx, 1920, 1080, "Hello world!");
 
-    mlx_hook(mlx_win, 2, 1L << 0, key_press, mlx);
-    mlx_hook(mlx_win, 17, 0, close_window, mlx);
-    
-    mlx_loop(mlx);
+    // img.img = mlx_xpm_file_to_image(game.mlx, relative_path, &img_width, &img_height);
+    // img.addr = mlx_put_image_to_window(game.mlx, game.win, &img, 0, 0);
+
+	mlx_hook(game.win, EVENT_KEYPRESS, 1L<<0, key_press_hndlr, &game);
+    mlx_hook(game.win, EVENT_CLOSEWINDOW, 0, close_window, &game);
+    mlx_loop(game.mlx);
+    return(0);
 }
